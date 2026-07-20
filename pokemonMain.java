fdc8f94fd5da7b7687645dd8c1b91e1c6e78c5f6
ricardocosmico <jeffthepeep80@gmail.com>
2026-07-20 08:47:02 -0300

    Create pokemonMain.java

---
+++ pokemonMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemon;
+
+/**
+ *
+ * @author curso
+ */
+public class PokemonMain {
+    
+     public static void main(String[] args) {
+    
+    Pokemon equipe = new Pokemon[2];
+    
+   Pokemon.equipe[0]="Pikachu", "Eletrico", 15, 80;
+   Pokemon.equipe[1]="Charmander", "Fogo", 12, 75;
+   Pokemon.equipe[2]="Squirtle", "Agua", 14, 90;
+   
+   equipe.exibirEquipe(equipe);
+    
+}
+}
+    
